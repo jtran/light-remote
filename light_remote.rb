87b11297f2651a45c73afe23c7069cd9c3e5f775
Jonathan Tran <jonnytran@gmail.com>
2010-10-27 22:19:36 -0400

    Lots of refactoring.  Not using sensor input, for now.

--- light_remote.rb
+++ light_remote.rb
@@ -4,37 +4,43 @@ require 'serialport'
 require 'osc'
 
 class LightRemote
-  Host = '192.168.0.2'
-  Port = 2222
 
-  def initialize
-    @sp = SerialPort.new("/dev/tty.usbserial-A600akVG", 19200)
+  attr_accessor :host, :port, :osc
+
+  def initialize(host, port, use_serial=true)
+    @host = host
+    @port = port
+    @sp = SerialPort.new("/dev/tty.usbserial-A600akVG", 19200) if use_serial
     @osc = OSC::UDPSocket.new
   end
 
   def send_light(r, g, b)
     #puts "sending #{r}, #{g}, #{b}"
-    d = 400.0
-    r = r / d
-    g = g / d
-    b = b / d
-    m = OSC::Message.new('/light/color/set', 'fff', r, g, b)
-    @osc.send(m, 0, Host, Port)
+    m = OSC::Message.new('/foo', 'fff', r, g, b)
+    @osc.send(m, 0, @host, @port)
   end
 
-  def loop
+  def read_triple
     line = @sp.readline
     puts line
     vals = line.split(',')
-    return if vals.nil? || vals.size != 3
-
-    send_light(*vals.map(&:to_i))
+    return nil if vals.nil? || vals.size != 3
+    vals.map(&:to_i)
   end
 
   def run
-    loop while true
+    while true do
+      triple = read_triple
+      send_light(*triple.map {|v| v / 400.0 }) if triple
+    end
   end
 
 end
 
-LightRemote.new.run
+l = LightRemote.new(ARGV[0] || '192.168.1.162', 2222, false)
+p l
+
+255.times do |r|
+  l.send_light(r / 255.0, 0.5, 0.5)
+  sleep(0.1)
+end
